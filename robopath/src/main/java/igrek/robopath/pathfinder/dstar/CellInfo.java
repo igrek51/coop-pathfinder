7791a7438b3d422e80cb1426a634bd71f54b58ee
igrek <igrek.s@o2.pl>
2017-12-17 00:33:18 +0100

    add D* Lite algorithm test

---
+++ robopath/src/main/java/igrek/robopath/pathfinder/dstar/CellInfo.java
@@ -0,0 +1,7 @@
+package igrek.robopath.pathfinder.dstar;
+
+public class CellInfo implements java.io.Serializable {
+	public double g = 0;
+	public double rhs = 0;
+	public double cost = 0;
+}
---
+++ robopath/src/main/java/igrek/robopath/pathfinder/dstar/Ipoint2.java
@@ -0,0 +1,17 @@
+package igrek.robopath.pathfinder.dstar;
+
+public class Ipoint2 {
+	public int x = 0;
+	public int y = 0;
+	
+	//default constructor
+	public Ipoint2() {
+	
+	}
+	
+	//overloaded constructor
+	public Ipoint2(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
